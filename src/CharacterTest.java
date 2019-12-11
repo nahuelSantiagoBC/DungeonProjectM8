313b90edec66dd7ddbfaac4af6fbf1b656e0f036
Ionut <ionut.alxu@gmail.com>
2019-12-11 20:40:54 +0100

    Character test (en proceso)

---
+++ src/CharacterTest.java
@@ -0,0 +1,30 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CharacterTest {
+
+    @Test
+    void attack() {
+    }
+
+    @Test
+    void defend() {
+    }
+
+    @Test
+    void die() {
+    }
+
+    @Test
+    void getExtraDefense() {
+    }
+
+    @Test
+    void setExtraDefense() {
+    }
+
+    @Test
+    void getInventory() {
+    }
+}
